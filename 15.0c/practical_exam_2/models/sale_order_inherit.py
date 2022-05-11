98d06e411de35f784476237e5c18a0492cb9e21a
suvas <suvaskumar@icreativetechnolabs.com>
2022-05-11 18:25:25 +0530

    Complit Exam Second pepaer

--- 15.0c/practical_exam_2/models/sale_order_inherit.py
+++ 15.0c/practical_exam_2/models/sale_order_inherit.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime
 from odoo import fields, models, api,_
+from odoo.exceptions import UserError, ValidationError
+
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -12,59 +13,55 @@ class SaleOrder(models.Model):
         rec = self.env['ir.config_parameter'].sudo()
         self.promotional_discount_check = rec.get_param('practical_exam_2.promotional_discount')
 
-
     def action_apply_promotional_discount(self,):
-
         promotional_discount = self.env['promotional.discount'].search([])
-        a=self.id
-        b=self.browse([a])
-        print('\n\n',b)
-        print('\n\norder',b.name,'\n\n',b.order_line)
-
-        active_id = self.env['sale.order']._context.get('active_id')
-        print('\n\n',active_id)
-        # read_list = self.env['sale.order']
-        read_list=b.search([])
-
-        list1 = (read_list.read(['order_line']))
-        for rec in list1:
-            print(rec)
-
-        print('\n\npromotional_discount', promotional_discount)
+        order_product_ids = self.order_line.product_id
+        product_ids_list = [product.id for product in order_product_ids]
         discount_id = self.env.ref("practical_exam_2.product_category_offers").id
-        # if self.env['sale.order'].search([discount_id, 'in', 'order_line']):
-        #     print('\n\n\nHello', 'Yellow')
-        print('\n\ndiscount_id', discount_id, '\n\nres',)
+        for offer in product_ids_list:
+            if discount_id == offer:
+                raise ValidationError(_('Sorry, You can add one discount coupon...'))
+
 
         discount_rec = promotional_discount.filtered(lambda record:
                                                      self.date_order >= record.start_date
                                                      and self.date_order <= record.end_date
                                                      and self.amount_untaxed >= record.minimum_order_amount)
-        print('\n\nello',discount_rec)
-        print('\n\namount_untaxed', self.amount_untaxed)
-        fix_price = [dis_price for dis_price in discount_rec if dis_price.discount_type == 'fixed_amount']
-        for fix_price_rec in discount_rec:
-            if fix_price_rec.discount_type == 'fixed_amount':
-                min_of_fix_dic = min(fix_price)
-        print('\n\n\nb', min_of_fix_dic)
+        discount_rec_error = [res.id for res in discount_rec]
+        if len(discount_rec_error) == 0:
+            raise ValidationError(_('Sorry, You are not applicable for any discount coupon...'))
 
         discount_percentage = [(self.amount_untaxed*i.discount)/100 for i in discount_rec if i.discount_type == 'percentage']
+        per_name_list = [p_dis_price for p_dis_price in discount_rec if p_dis_price.discount_type == 'percentage']
+        min_p = min([res.discount for res in per_name_list])
+        per_name = (self.env['promotional.discount'].search([('discount', '=', min_p), ('discount_type', '=', 'percentage')]))
+
         for rec in discount_rec:
             if rec.discount_type == 'percentage':
+                fix_price = [dis_price for dis_price in discount_rec if dis_price.discount_type == 'fixed_amount']
                 min_percentage = min(discount_percentage)
-                if min_of_fix_dic.discount > min_percentage:
+                min_per = -(min_percentage)
+                if len(fix_price) == 0:
                     self.write({'order_line': [(0, 0, {'product_id': discount_id,
-                                               'price_subtotal': min_percentage})]})
-                    print('heyy')
+                                                       'name': per_name.name,
+                                                       'price_unit': min_per})]})
                     break
                 else:
-                    discount = -(min_of_fix_dic.discount)
-                    print("\n\n", type(discount), discount)
-                    self.write({'order_line': [(0, 0, {'product_id': discount_id,
-                                                       'price_unit': discount})]})
-                    print('How')
-                    break
-        print('\n\n\nd', min_percentage, '\n\n')
+                    min_fix_dic = min([res.discount for res in fix_price])
+                    fix_dic_name = (self.env['promotional.discount'].search(
+                        [('discount', '=', min_fix_dic), ('discount_type', '=', 'fixed_amount')]))
+                    if min_fix_dic > min_percentage:
+                        self.write({'order_line': [(0, 0, {'product_id': discount_id,
+                                                           'name': per_name.name,
+                                                   'price_unit': min_percentage})]})
+                        break
+                    else:
+                        discount = -(min_fix_dic)
+                        print("\n\n", type(discount), discount)
+                        self.write({'order_line': [(0, 0, {'product_id': discount_id,
+                                                           'name': fix_dic_name.name,
+                                                           'price_unit': discount})]})
+                        break
 
 
 
